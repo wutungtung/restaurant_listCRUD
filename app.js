9fc937225f968df0b72990ca773b3d11f0b63b3a
wutungtung <112239752+wutungtung@users.noreply.github.com>
2023-10-31 23:28:29 +0800

    initialize project

--- app.js
+++ app.js
@@ -1,28 +1,8 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
-const restaurantData = require("./models/restaurant.json");
+const restaurantData = require("./restaurant.json");
 const app = express();
 const port = 3000;
-const mongoose = require("mongoose");
-
-if (process.env.NODE_ENV !== "production") {
-  require("dontenv").config();
-}
-
-mongoose.connection(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;
-
-db.on("error", () => {
-  console.log("mongoDB error!");
-});
-
-db.once("open", () => {
-  console.log("mongoDB connected!");
-});
 
 //express template engine
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
